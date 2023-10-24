a3f2d5910b54f8e6be92dd3f781f82bd8679f514
tbeaumont79 <tbeaumont79@icloud.com>
2023-10-24 19:27:53 +0200

    working on learning a bit more of tailwind and font

--- VueSandBox/src/components/Home.vue
+++ VueSandBox/src/components/Home.vue
@@ -1,20 +1,26 @@
 <script setup lang="ts"></script>
 <template>
-  <div class="flex h-screen flex-row justify-between">
-    <div class="relative overflow-hidden w-1/2 p-12 flex1 flex-col">
-      <img
-        class="absolute inset-0 h-full w-full object-cover rounded-lg"
-        src="../assets/thibault.png"
-        alt="picture of thibault"
-      />
-    </div>
-    <div class="flex2 flex-col w-1/2 justify-center px-4 py-12">
-      <h1 class="text-5xl font-bold leading-9 tracking-tight text-gray-900">
-        Hi. I'm Thibault a Front end developer
-      </h1>
-      <p class="mt-4 text-lg font-bold leading-7 text-gray-500">
-        I love bringing ideas to life by building websites
-      </p>
+  <div class="container mx-auto px-6 p-10">
+    <div class="flex space-y-8 justify-evenly">
+      <div class="w-1/2">
+        <img
+          class="object-contain rounded-lg"
+          src="../assets/thibault.png"
+          alt="picture of thibault"
+        />
+      </div>
+      <div class="flex2 flex-col w-1/3 justify-center">
+        <h1
+          class="text-5xl font-bold font-poppins leading-9 tracking-normal text-gray-900"
+        >
+          Hi. I'm Thibault a Front end developer
+        </h1>
+        <p
+          class="mt-8 text-lg font-poppins font-bold tracking-wide leading-8 text-gray-500"
+        >
+          I love bringing ideas to life by building websites
+        </p>
+      </div>
     </div>
   </div>
 </template>
